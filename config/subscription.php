3b25dcd9c184862e2443c6df5fe74e857ac60a7b
Foster Amponsah <foster.asante@gwosevo.com>
2025-12-15 15:17:26 +0000

    Update

---
+++ config/subscription.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'api_key' => env('YOUR_SDK_API_KEY', 'default_key'), // default_key is a fallback value
+    'api_url' => env('YOUR_SDK_API_URL', 'https://example.com/api'), // Default API URL
+];
